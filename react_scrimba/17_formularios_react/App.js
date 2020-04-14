d20418e6607bb71465226026cb58e8e0be61eb2d
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-04-13 23:28:57 -0500

    formularios primeros tipos

---
+++ react_scrimba/17_formularios_react/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+
+// Documentacion de React
+// https://reactjs.org/docs/forms.html
+
+
+/**
+ * Los formularios en React son especiales porque acostumbran a tener una continua actualizacion de su informacion
+ * Esta informacion se guarda en el state de la siguiente manera segun su documentacion
+ */
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nombre: ''
+    }
+    // Creamos un metodo que este ligado a los cambios del input
+    this.handleNombre = this.handleNombre.bind(this)
+  }
+
+
+  // Este metodo recibira la informacion del input
+  handleNombre(event) {
+    const nombre = event.target.value;
+    // Esta informacion sera guardada en el state
+    this.setState({ nombre })
+  }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <form>
+  //         <input type="text" placeholder="First Name" onChange={ this.handleNombre } />
+  //       </form>
+  //       <div>
+  //         { this.state.nombre }
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+  
+  /**
+   * Esta implementacion funcionaria para un solo input o dos pero que pasaria si tuviera un formulario con
+   * mas de 10 inputs o queremos ordenarlos pues usaremos lo siguiente
+   */
+
+  // Se manejaria con el siguiente handler
+  handleInputEvents(event) {
+    this.setState({
+      [event.target.name]: event.target.value // Esto se puede por https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="nombre" placeholder="Nombre " onChange={ this.handleNombre } />
+          <input type="text" name="apellido" placeholder="Apellido" onChange={ this.handleNombre } />
+          <input type="text" name="edad" placeholder="Edad" onChange={ this.handleNombre } />
+        </form>
+        <div>
+          { this.state.nombre }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
+
